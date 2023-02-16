d514b9e83ca13183f3bce04783cbbe40b2683175
Peter Lee <PeterLee2094@gmail.com>
2023-02-16 01:06:24 -0600

    added input fields

--- src/components/AddMovieModal/AddMovieModal.jsx
+++ src/components/AddMovieModal/AddMovieModal.jsx
@@ -3,7 +3,26 @@ export default function AddMovieModal(){
     return(
         <div className={styles.background}>
             <div className={styles.container}>
-
+                <div>
+                    <p>Title</p>
+                    <input/>
+                </div>
+                <div>
+                    <p>Genres</p>
+                    <input type="checkbox" value="Adventure"/>
+                    <input type="checkbox" value=""/>
+                    <input type="checkbox" value=""/>
+                    <input type="checkbox" value=""/>
+                    <input type="checkbox" value=""/>
+                    <input type="checkbox" value=""/>
+                    <input type="checkbox" value=""/>
+                    <input type="checkbox" value=""/>
+                    <input type="checkbox" value=""/>
+                </div>
+                <div>
+                    <p></p>
+                    <textarea/>
+                </div>
             </div>
         </div>
     )
