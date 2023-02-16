5250387dfbcbe38f571a95fa5c2eb073c4275329
Peter Lee <PeterLee2094@gmail.com>
2023-02-16 01:21:54 -0600

    added get all genre

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -10,14 +10,29 @@ router.get("/:id", (req, res) => {
   JOIN "movies" ON "movies"."id" = "movies_genres"."movie_id"
   WHERE "movies"."id" = $1;`;
 
-  pool.query(queryText, [movieId.id])
-  .then( result => {
-    res.send(result.rows);
-  })
-  .catch(err => {
-    console.log('ERROR: Get all movies', err);
-    res.sendStatus(500)
-  })
+  pool
+    .query(queryText, [movieId.id])
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("ERROR: Get all movies", err);
+      res.sendStatus(500);
+    });
+});
+
+router.get("/", (req, res) => {
+  const queryText = `SELECT "name" FROM "genres"`;
+  pool
+    .query(queryText)
+    .then((response) => {
+      console.log(response.rows)
+      res.send(response.rows);
+    })
+    .catch((error) => {
+      console.log("Error with getting all genre, ", error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
--- src/components/AddMovieModal/AddMovieModal.jsx
+++ src/components/AddMovieModal/AddMovieModal.jsx
@@ -1,5 +1,15 @@
-import styles from './AddMovieModal.module.css'
+import styles from './AddMovieModal.module.css';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import { useEffect } from 'react';
 export default function AddMovieModal(){
+    const dispatch = useDispatch();
+
+    const fetchGenre = () => {
+        dispatch({type:"FETCH_ALL_GENRE"});
+    }
+
+    useEffect(()=>{fetchGenre()},[]);
     return(
         <div className={styles.background}>
             <div className={styles.container}>
@@ -22,6 +32,10 @@ export default function AddMovieModal(){
                 <div>
                     <p></p>
                     <textarea/>
+                </div>
+                <div>
+                    <button>add movie</button>
+                    <button>cancel</button>
                 </div>
             </div>
         </div>
