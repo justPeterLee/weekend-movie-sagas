07a6c94e9938d359973a96d688d560a920d22c3d
Peter Lee <PeterLee2094@gmail.com>
2023-02-16 00:38:44 -0600

    changed addmovie to button

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -3,9 +3,9 @@ import styles from './AddMovie.module.css';
 export default function AddMovie(){
     return(
         <div className={styles.container}>
-            <div className={styles.circle}>
+            <button className={styles.circle}>
                 <h1 className={styles.plus}>+</h1>
-            </div>
+            </button>
         </div>
     )
 }
