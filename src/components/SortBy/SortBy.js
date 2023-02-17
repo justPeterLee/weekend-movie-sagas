68d958016becba66b831a9217ac37edc564c55c3
Peter Lee <PeterLee2094@gmail.com>
2023-02-17 00:25:19 -0600

    added html template and fetch all genres

--- src/components/SortBy/SortBy.js
+++ src/components/SortBy/SortBy.js
@@ -1,5 +1,21 @@
+import styles from './Sortby.module.css'
+import { useDispatch, useSelector } from 'react-redux'
 export default function SortBy(){
-    return{
-        
-    }
+    const dispatch = useDispatch();
+    const genres = useSelector((store) => store.allGenres);
+
+    const fetchGenre = () => {
+        dispatch({ type: "FETCH_ALL_GENRE" });
+      };
+      useEffect(() => {
+        fetchGenre();
+      }, []);
+    return(
+        <div>
+            <form>
+                <label>genre: </label>
+                <option></option>
+            </form>
+        </div>
+    )
 }
